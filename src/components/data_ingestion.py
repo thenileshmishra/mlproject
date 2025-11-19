c18d0354c3d9392c465b1190c96564fa467831f7
thenileshmishra <thenileshmishra@gmail.com>
2025-11-19 13:22:45 +0530

    data_tranformation part is implemented

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from src.exception import CustomException
-from src.logger import logging   # ensure this is a Logger object
+from src.logger import logging   
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join("artifacts", "train.csv")
@@ -62,3 +64,6 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion("/Users/nileshmishra/MLOPs/notebook/data/StudentsPerformance.csv")
     print(f"Train data path: {train_data}")
     print(f"Test data path: {test_data}")
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
